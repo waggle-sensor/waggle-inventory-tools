6feac70f583478de404d6d1834964ca917906a49
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-11-13 16:28:49 -0600

    format output

--- print-devices.py
+++ print-devices.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import json
 
 
-print("vsn", "device", "zone", "cpu", "memory", sep="\t")
+print("vsn      device                           zone         cpu     memory    notes")
 
 for path in Path("data").glob("*/kube-nodes.json"):
     vsn = path.parent.stem
@@ -13,4 +13,12 @@ for path in Path("data").glob("*/kube-nodes.json"):
         zone = item["metadata"]["labels"].get("zone", "unknown")
         cpu = item["status"]["capacity"]["cpu"]
         memory = int(item["status"]["capacity"]["memory"].removesuffix("Ki")) * 1024
-        print(vsn, name, zone, cpu, round(memory / 1024**3, 3), sep="\t")
+
+        notes = []
+
+        if not Path("data", vsn, "devices", name).exists():
+            notes.append("unreachable")
+
+        print(
+            f"{vsn:8} {name:32} {zone:12} {cpu:4} {memory / 1024**3:8.3f}     {','.join(notes)}"
+        )

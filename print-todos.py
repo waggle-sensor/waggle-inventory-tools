9cd4d95919b8d525520b7af40b40ce839e95ef8e
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-11-13 11:36:59 -0600

    update

--- print-todos.py
+++ print-todos.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3
 from pathlib import Path
 import json
 
 # Example of finding all devices missing a zone.
-for path in Path("data").glob("kube-nodes.json"):
+for path in Path("data").glob("*/kube-nodes.json"):
     vsn = path.parent.stem
     kube_nodes_data = json.loads(path.read_text())
     for item in kube_nodes_data["items"]:
         name = item["metadata"]["name"]
         zone = item["metadata"]["labels"].get("zone")
         if not zone:
-            print(f"Missing zone for {vsn} {name}")
+            print(f"ERROR: Missing zone for {vsn} {name}")
         if zone not in ["core", "agent", "shield", "enclosure"]:
-            print(f"Invalid zone for {vsn} {name}: {zone}")
+            print(f"ERROR: Invalid zone for {vsn} {name}: {zone}")
+        if not Path("data", vsn, "devices", name).exists():
+            print("WARNING: Device in k3s but not scraped", vsn, name)
